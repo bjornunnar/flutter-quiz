799c1298d8a846a799658c1366c7675391713151
Björn Unnar <35260726+bjornunnar@users.noreply.github.com>
2023-11-08 20:43:24 +0000

    update quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,60 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:quiz/questions.dart';
 import 'package:quiz/start_screen.dart';
 
-
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState(){ 
-  return _QuizState();
-
+  State<Quiz> createState() {
+    return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
-
-  Widget? activeScreen;
+  String? activeScreen;
 
   // initState is a built in function that runs one time, after the class is built.
-  @override
-  void initState(){
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // this is an example
+  // -- instead, we are using the screenWidget variable within the build method, with an if statement
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
 
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = "questions-screen";
     });
   }
 
   @override
-  Widget build(context){
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == "questions-screen") {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 78, 13, 151),
-              Color.fromARGB(255, 107, 15, 168)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        ),
-          child: activeScreen,
+          child: screenWidget,
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
